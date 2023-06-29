c242e65c26782d5efdcb798d35c1a748b6096b02
ACHTK <yemengzhidiandig@gmail.com>
2023-06-29 21:16:33 +0800

    no commit message

---
+++ vue-support-maintain-starter/src/view/terminal.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <terminal name="my-terminal" @exec-cmd="onExecCmd"></terminal>
+</template>
+
+<script>
+import Terminal from "vue-web-terminal"
+
+export default {
+  name: 'Terminal-editor',
+  components: {Terminal},
+  methods: {
+    onExecCmd(key, command, success, failed) {
+      if (key === 'fail') {
+        failed('Something wrong!!!')
+      } else {
+        let allClass = ['success', 'error', 'system', 'info', 'warning'];
+
+        let clazz = allClass[Math.floor(Math.random() * allClass.length)];
+        success({
+          type: 'normal',
+          class: clazz,
+          tag: '成功',
+          content: command
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
