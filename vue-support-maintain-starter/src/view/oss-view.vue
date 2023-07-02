49d5f19389db1fd486d3f3fd1c7c0941d41731f4
ACHTK <yemengzhidiandig@gmail.com>
2023-07-02 22:00:19 +0800

    no commit message

--- vue-support-maintain-starter/src/view/oss-view.vue
+++ vue-support-maintain-starter/src/view/oss-view.vue
@@ -178,49 +178,6 @@ export default defineComponent({
         }
     },
     methods: {
-        initialVideo: function() {
-            let _this = this; 
-            this.ossData.forEach((element,i) => {
-                if(element.type !== 'video') {
-                    return;
-                }
-               
-                this.videoOptions[element.id] = {
-                    //播放速度
-                    playbackRates: [0.5, 1.0, 1.5, 2.0], 
-                    //如果true,浏览器准备好时开始回放。
-                    autoplay: false, 
-                    // 默认情况下将会消除任何音频。
-                    muted: false, 
-                    // 导致视频一结束就重新开始。
-                    loop: false, 
-                    // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-                    preload: 'auto', 
-                    language: 'zh-CN',
-                    // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-                    aspectRatio: '16:9',
-                    // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-                    fluid: true,
-                    sources: [{
-                        //类型
-                        type: "video/" + element.subtype,
-                        //url地址
-                        src: this.prefix + '/' + this.base.ossBucket + '/' + this.element.name
-                    }],
-                    //你的封面地址
-                    poster: '', 
-                    //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-                    notSupportedMessage: '此视频暂无法播放，请稍后再试',
-                    controlBar: {
-                        timeDivider: true,
-                        durationDisplay: true,
-                        remainingTimeDisplay: false,
-                        //全屏按钮
-                        fullscreenToggle: true  
-                    }
-                }
-            });
-        },
         getVideoOptions: function(row) {
             const t = _.cloneDeep(this.playerOptions);
             t.sources = [{
@@ -258,7 +215,6 @@ export default defineComponent({
                 this.total = data.data.total;
                 this.ossData.length = 0;
                 this.ossData = data.data.data;
-                this.initialVideo();
             }).catch(data => {
                 layx.notice({
                     title: '提示',
