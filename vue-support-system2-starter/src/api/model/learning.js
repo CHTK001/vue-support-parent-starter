cfa55e3b9f8fd5c9756d582380a3fd43d6931127
ACHTK <ACHTK@qq.com>
2023-07-31 19:34:56 +0800

    ocr

--- vue-support-system2-starter/src/api/model/learning.js
+++ vue-support-system2-starter/src/api/model/learning.js
@@ -24,4 +24,16 @@ export default {
 			return await http.post(this.url, data, config);
 		}
 	},
+	ocr: {
+        url: `${config.API_LEARNING}/v1/ocr`,
+		name: "OCR",
+		post: async function(data, config={
+			Headers: {
+				'ContentType': 'multipart/form-data'
+			}
+		}){
+			return await http.post(this.url, data, config);
+		}
+	},
+
 }
---
+++ vue-support-system2-starter/src/views/learning/ocr/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<el-container>
+		<el-aside style="width: 30%">
+			<el-container>
+				<el-main>
+                    <sc-upload :apiObj="apiObj" @onSuccess="onSuccess"  class="upload"   :compress="1" :aspectRatio="1/1">
+                            </sc-upload>
+                </el-main>
+			</el-container>
+		</el-aside>
+		<el-container style="width: 70%">
+			<el-main class="nopadding">
+                <el-empty :description="this.$t('data.nodata')" />
+			</el-main>
+		</el-container>
+	</el-container>
+</template>
+
+<script>
+	export default {
+		name: 'layoutLCR',
+		data() {
+			return {
+                apiObj: this.$API.learning.ocr
+			}
+		},
+        methods: {
+            onSuccess(d) {
+                debugger    
+            }
+        }
+	}
+</script>
+
+<style scoped lang="less">
+:deep(.el-upload--picture-card) {
+    width: 100% !important;
+    height: 100%  !important;
+}
+.percentage-value {
+  display: block;
+  margin-top: 10px;
+  font-size: 28px;
+}
+.percentage-label {
+  display: block;
+  margin-top: 10px;
+  font-size: 12px;
+}
+.upload {
+    width: 100% !important;
+    height: 100%  !important;
+}
+.upload-bottom {
+    border-bottom: 1px solid var(--el-border-color);
+}
+</style>
