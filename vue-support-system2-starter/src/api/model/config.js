1dfae9d44cca53aecce0e61b02ae1689c40b97f7
ACHTK <yemengzhidiandig@gmail.com>
2023-08-12 15:04:21 +0800

    统一配置

--- vue-support-system2-starter/src/api/model/config.js
+++ vue-support-system2-starter/src/api/model/config.js
@@ -5,32 +5,39 @@ export default {
     config: {
 		page: {
 			url: `${config.API_CONF}/v1/configuration/page`,
-			name: "获取映射",
-			get: async function () {
-				return await http.get(this.url);
+			name: "获取配置",
+			get: async function (params) {
+				return await http.get(this.url, params);
 			},
 		},
 		save: {
 			url: `${config.API_CONF}/v1/configuration/save`,
-			name: "添加映射",
-			post: function (params) {
-				return http.post(this.url, params);
+			name: "添加配置",
+			post: async function (params) {
+				return await http.post(this.url, params);
 			},
 		},
 		update: {
 			url: `${config.API_CONF}/v1/configuration/update`,
-			name: "更新映射",
-			put: function (params) {
-				return http.put(this.url, params);
+			name: "更新配置",
+			put: async function (params) {
+				return await http.put(this.url, params);
 			},
 		},
 		delete: {
 			url: `${config.API_CONF}/v1/configuration/delete`,
-			name: "删除映射",
-			delete: function (p) {
-				return http.delete(this.url + `?mapId=${p.libId}`, {
+			name: "删除配置",
+			delete: async function (p) {
+				return await http.delete(this.url + `?configId=${p.configId}`, {
 					params: p,
 				});
+			},
+		},
+		profile: {
+			url: `${config.API_CONF}/v1/configuration/profile`,
+			name: "环境",
+			get: async function (p) {
+				return await http.post(this.url);
 			},
 		},
 	},
--- vue-support-system2-starter/src/views/config/config/index.vue
+++ vue-support-system2-starter/src/views/config/config/index.vue
@@ -6,7 +6,8 @@
                 <br />
             </div>
             <div class="right-panel">
-                <el-button type="primary" icon="el-icon-plus"></el-button>
+                <el-button type="primary" icon="el-icon-search" @click="search"></el-button>
+                <el-button type="primary" icon="el-icon-plus" @click="table_edit({})"></el-button>
             </div>
         </el-header>
         <el-main class="nopadding">
@@ -27,9 +28,52 @@
                             style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949" />
                     </template>
                 </el-table-column>
+                <el-table-column label="操作" fixed="right" align="right" width="260">
+                    <template #default="scope">
+                        <el-button-group v-if="!scope.row.configName.startsWith('binder-')">
+                            <el-button v-auth="'sys:config:edit'" text type="primary" size="small" @click="table_edit(scope.row, scope.$index)">编辑</el-button>
+                            <el-popconfirm   v-auth="'sys:config:del'" title="确定删除吗？" @confirm="table_del(scope.row, scope.$index)">
+                                <template #reference>
+                                    <el-button  v-auth="'sys:config:del'" text type="primary" size="small">删除</el-button>
+                                </template>
+                            </el-popconfirm>
+                        </el-button-group>
+                    </template>
+                </el-table-column>
             </scTable>
         </el-main>
     </el-container>
+
+    <el-dialog draggable v-model="visible" :width="500" destroy-on-close @closed="$emit('closed')">
+		<el-form :model="form" :disabled="mode=='show'" ref="dialogForm" label-width="100px" label-position="left">
+			<el-form-item v-show="false" label="索引" prop="configName">
+				<el-input v-model="row.configId" clearable></el-input>
+			</el-form-item>
+			<el-form-item label="环境" prop="configProfile">
+                <el-select v-model="row.configProfile">
+                    <el-option v-for="it in profiles" :label="it"></el-option>
+                </el-select>
+			</el-form-item>
+			<el-form-item v-if="!row.configId" label="应用名称" prop="configItem">
+                <el-select v-model="row.configItem">
+                    <el-option v-for="it in applications" :label="it"></el-option>
+                </el-select>
+			</el-form-item>
+			<el-form-item  label="配置名称" prop="configName">
+				<el-input :readonly="row.configId" :disabled="row.configId" v-model="row.configName" clearable></el-input>
+			</el-form-item>
+            <el-form-item label="配置值" prop="configValue">
+				<el-input v-model="row.configValue" clearable></el-input>
+			</el-form-item>
+            <el-form-item label="描述" prop="configDesc">
+				<el-input v-model="row.configDesc" clearable></el-input>
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<el-button @click="visible=false" >取 消</el-button>
+			<el-button v-if="mode!='show'" type="primary" :loading="isSaveing" @click="submitFormUpdate()">保 存</el-button>
+		</template>
+	</el-dialog>
 </template>
 
 <script>
@@ -48,39 +92,24 @@ export default {
             form: {
                 mapMethod: []
             },
+            visible: 0,
+            searchParams: {},
             data: [
                 {
-                    title: "类型(多)",
-                    key: "mapMethod",
+                    title: "环境",
+                    key: "configProfile",
                     multiple: !1,
                     options: [
                         {
                             label: "全部",
                             value: ""
                         },
-                        {
-                            label: "GET",
-                            value: "GET"
-                        },
-                        {
-                            label: "POST",
-                            value: "POST"
-                        },
-                        {
-                            label: "PUT",
-                            value: "PUT"
-                        },
-                        {
-                            label: "DELETE",
-                            value: "DELETE"
-                        },
-                        {
-                            label: "PATCH",
-                            value: "PATCH"
-                        }
                     ]
                 }
             ],
+            row: {},
+            profiles: [],
+            applications: [],
             list: {
                 apiObj: this.$API.config.config.page,
                 apiObjUpdate: this.$API.config.config.update,
@@ -89,14 +118,47 @@ export default {
             }
         }
     },
+    mounted(){
+        this.initial();
+    },
     methods: {
-        submitFormUpdate(row) {
-            this.list.apiObjUpdate.update({
-                mapId: row.mapId,
-                mapApplicationStatus: row.mapApplicationStatus
-            }).then(res => {
+        search() {
+            this.$refs.table.reload(this.searchParams)
+        },
+        table_del(row) {
+            this.list.apiObjDelete.delete(row).then(res => {
                 if(res.code === '00000') {
                     this.$message.success("操作成功");
+                    this.search();
+                    this.visible = !1;
+                    return 0;
+                } 
+                this.$message.error(res.msg);
+            })
+        },
+        table_edit(row) {
+            this.visible = !0;
+            Object.assign(this.row, row);
+        },
+        async initial(){
+            const res = await this.$API.config.config.profile.get();
+            if(res.code === '00000') {
+                this.profiles = res.data;
+                res.data.forEach(item => {
+                    this.data[0].options.push({
+                        label: item,
+                        value: item
+                    })
+                })
+            }
+
+        },
+        submitFormUpdate() {
+            this.list.apiObjSave.post(this.row ).then(res => {
+                if(res.code === '00000') {
+                    this.$message.success("操作成功");
+                    this.search();
+                    this.visible = !1;
                     return 0;
                 } 
                 this.$message.error(res.msg);
@@ -107,6 +169,7 @@ export default {
             return row[property] === value
         },
         change(selected) {
+            this.searchParams = selected;
             this.$refs.table.reload(selected)
         }
     }
