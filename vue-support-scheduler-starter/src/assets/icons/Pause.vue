8425d2cfc4776d376ea245c4b3c13905a5d0923b
ACHTK <yemengzhidiandig@gmail.com>
2023-09-12 21:26:56 +0800

    no commit message

---
+++ vue-support-scheduler-starter/src/assets/icons/Pause.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg t="1690042557840" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+        p-id="27930" width="200" height="200">
+        <path
+        d="M512 1024A512 512 0 1 1 512 0a512 512 0 0 1 0 1024z m3.008-92.992a416 416 0 1 0 0-832 416 416 0 0 0 0 832zM320 320h128v384H320V320z m256 0h128v384H576V320z"
+        fill="#262626" p-id="27931"></path>
+</svg></template>
---
+++ vue-support-scheduler-starter/src/config/select.js
@@ -0,0 +1,6 @@
+import API from "@/api";
+
+//字典选择器配置
+
+export default {
+}
