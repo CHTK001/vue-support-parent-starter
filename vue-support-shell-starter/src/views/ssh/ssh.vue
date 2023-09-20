8859618399d3094a52dbd19bd8d0c0852a69c325
ACHTK <yemengzhidiandig@gmail.com>
2023-09-20 20:53:03 +0800

    ssh

--- vue-support-shell-starter/src/views/ssh/ssh.vue
+++ vue-support-shell-starter/src/views/ssh/ssh.vue
@@ -1,10 +1,11 @@
 <template>
-       <div id="xterm" class="xterm" />
+       <div id="xterm" class="xterm" style="height: 100vh; width: 100%;"/>
 </template>
 <script>
 import { Terminal } from 'xterm';
 import { FitAddon } from "xterm-addon-fit";
 import { AttachAddon  } from "xterm-addon-attach";
+import { WebLinksAddon  } from "xterm-addon-web-links";
 import "xterm/css/xterm.css";
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
@@ -29,27 +30,30 @@ export default {
         //连接打开事件
         this.websocket.onclose = this.closeSocket;
         this.websocket.onopen = this.openSocket;
+        window.addEventListener('resize', this.onTerminalResize);
         this.websocket.onmessage = this.openMessage;
     },
     methods: {
         initTerm() {
             this.term = new Terminal({
-                lineHeight: 1.2,
-                fontSize: 12,
                 fontFamily: "Monaco, Menlo, Consolas, 'Courier New', monospace",
-                theme: {
-                    background: '#181d28',
+                cursorBlink: true, // 光标闪耀
+                allowProposedApi: true,
+                disableStdin: false, //是否应禁用输入
+                cursorStyle: "underline", //光标款式
+                windowsMode: true,// 根据窗口换行
+                theme: { // 设置主题
+                    foreground: "yellow", //字体
+                    background: "#060101", //背景色
+                    cursor: "help", //设置光标
                 },
-                // 光标闪烁
-                cursorBlink: true,
-                cursorStyle: 'underline',
-                scrollback: 100,
-                tabStopWidth: 4,
             });
            
             const fitAddon = new FitAddon();
+            const webLinksAddon = new WebLinksAddon();
             const attachAddon  = new AttachAddon(this.websocket);
             this.term.loadAddon(fitAddon);
+            this.term.loadAddon(webLinksAddon);
             this.term.loadAddon(attachAddon);
             fitAddon.fit()
             this.term.open(document.getElementById("xterm"));
@@ -59,10 +63,19 @@ export default {
         closeSocket() {
             console.log("Socket 已关闭");
         },
-        openMessage(msg) {
-            //将返回的消息写入终端
-            this.term.write(msg);
-        },
+        onTerminalResize(){
+            const terminalContainer = this.terminalContainerRef.current;
+            const width = terminalContainer.parentElement.clientWidth;
+            const height = terminalContainer.parentElement.clientHeight;
+            const { xterm } = this;
+            // 计算cols，rows
+            const cols = (width - xterm._core.viewport.scrollBarWidth - 15) / xterm._core._renderService._renderer.dimensions.actualCellWidth;
+            const rows = height / xterm._core._renderService._renderer.dimensions.actualCellHeight - 1;
+            this.xterm.resize(
+                parseInt(cols.toString(), 10),
+                parseInt(rows.toString(), 10)
+            );
+        }
     }
 }
 </script>
