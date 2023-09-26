eae93933a9a6fd67e448ae5f30f4c603a579bccf
ACHTK <ACHTK@qq.com>
2023-09-26 17:02:03 +0800

    1

--- vue-support-gen-starter/src/views/gen/console/log.vue
+++ vue-support-gen-starter/src/views/gen/console/log.vue
@@ -1,5 +1,26 @@
 <template>
-    
+    <el-container>
+        <el-main>
+            <div ref="containerRef" style="height: 100%; overflow: auto;" @keyup.native="keyEvent">
+                <ul>
+                    <li v-for="item in data">
+                        <span v-html="item"></span>
+                    </li>
+                </ul>
+
+                <el-empty v-if="!data || data.length == 0" />
+
+            </div>
+
+            <el-button type="primary" icon="el-icon-search"
+                style="position: fixed; right: 0; top: 50%; width: 40px; height: 40px;" @click="showFile = !0"></el-button>
+
+            <el-dialog draggable v-model="showFile">
+                <el-input ref="input" v-model="input" placeholder="搜索" size="large" clearable prefix-icon="el-icon-search"
+                    @keyup.enter="enterQuery" :trigger-on-focus="false" />
+            </el-dialog>
+        </el-main>
+    </el-container>
 </template>
 <script>
 import {EventSourcePolyfill } from "event-source-polyfill"
@@ -10,6 +31,9 @@ export default {
     data() {
         return {
             form: {},
+            input: '',
+            showFile: 0,
+            data: [],
             eventSource: null
         }
     },
@@ -22,7 +46,6 @@ export default {
     },
     mounted(){
         this.form.tabId = this.$route.params.tabId;
-        this.addTemplate.tabId =  this.form.tabId;
         this.form.genId = this.$route.params.genId;
         if(!this.form.tabId || this.form.tabId === 'null') {
             delete  this.form.tabId;
@@ -30,7 +53,7 @@ export default {
         if(!this.form.genId || this.form.genId === 'null') {
             delete  this.form.genId;
         }
-        this.initial();
+        this.subscribe(!!this.form.tabId ? "TABLE" : "DATABASE");
     },
     methods: {
         subscribe: function (mode) {
@@ -41,7 +64,7 @@ export default {
                     this.eventSource.close();
                 } catch (e) { }
             }
-            this.eventSource = new EventSourcePolyfill(this.$API.table.subscribe.url + mode);
+            this.eventSource = new EventSourcePolyfill(this.$API.gen.table.subscribe.url + (this.form.tabId || this.form.genId)+ "/" + mode);
             this.eventSource.addEventListener(mode, (event) => {
                 const data = JSON.parse(event.data);
                 this.data.push(ansi_up.ansi_to_html(data.message).replaceAll('\n', '<br/>'));
