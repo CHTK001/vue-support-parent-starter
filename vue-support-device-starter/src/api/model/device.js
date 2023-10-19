3039d6d34f0eb60ca50de9e4a6ed88549847ee80
ACHTK <ACHTK@qq.com>
2023-10-19 10:39:36 +0800

    11

--- vue-support-device-starter/src/api/model/device.js
+++ vue-support-device-starter/src/api/model/device.js
@@ -4,35 +4,35 @@ import http from "@/utils/request"
 export default {
 	dict: {
         save: {
-			url: `${config.API_CONF}/v1/dict/save`,
+			url: `${config.API_CONF}/v1/device/dict/save`,
 			name: "保存厂家配置",
 			post: async function(data, config={headers:{}}){
 				return await http.post(this.url, data, config);
 			}
 		},
 		update: {
-			url: `${config.API_CONF}/v1/dict/update`,
+			url: `${config.API_CONF}/v1/device/dict/update`,
 			name: "更新厂家配置",
 			put: async function(data, config={headers:{}}){
 				return await http.put(this.url, data, config);
 			}
 		},
 		delete: {
-			url: `${config.API_CONF}/v1/dict/delete`,
+			url: `${config.API_CONF}/v1/device/dict/delete`,
 			name: "删除厂家配置",
 			delete: async function(data, config={headers:{}}){
 				return await http.delete(this.url + `?id=${data.id}` , data, config);
 			}
 		},
 		page: {
-			url: `${config.API_CONF}/v1/dict/page`,
+			url: `${config.API_CONF}/v1/device/dict/page`,
 			name: "查询厂家信息",
 			get: async function(data, config={headers:{}}){
 				return await http.get(this.url , data, config);
 			}
 		},
         list: {
-			url: `${config.API_CONF}/v1/dict/list`,
+			url: `${config.API_CONF}/v1/device/dict/list`,
 			name: "查询厂家信息",
 			get: async function(data, config={headers:{}}){
 				return await http.get(this.url , data, config);
