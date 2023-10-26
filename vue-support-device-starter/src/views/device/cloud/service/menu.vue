fe53ee5789206908c106544d0d39b8492dea9817
ACHTK <yemengzhidiandig@gmail.com>
2023-10-26 19:54:54 +0800

    1.提供公共接口

--- vue-support-device-starter/src/views/device/cloud/service/menu.vue
+++ vue-support-device-starter/src/views/device/cloud/service/menu.vue
@@ -1,8 +1,8 @@
 <template>
-    <el-button style="margin-left: 0px;" v-if="this.groupMap['device'] " 
-    type="primary" size="small" icon="sc-icon-device" text plain @click="doDowload(item)" :title="this.groupMap['device']['desc']"></el-button>
+    <el-button :loading="loadDeviceStatus" style="margin-left: 0px;" v-if="this.groupMap['device'] " 
+    type="primary" size="small" icon="sc-icon-device" text plain @click="syncDevice(item)" :title="this.groupMap['device']['desc']"></el-button>
 
-    <el-button style="margin-left: 0px;" v-if="this.groupMap['org'] " 
+    <el-button :loading="loadOrgStatus" style="margin-left: 0px;" v-if="this.groupMap['org'] " 
     type="primary" size="small" icon="sc-icon-org" text plain @click="syncOrg(item)" :title="this.groupMap['org']['desc']"></el-button>
 </template>
 <script>
@@ -18,6 +18,8 @@ export default {
     },
     data() {
         return {
+            loadDeviceStatus: false,
+            loadOrgStatus: false,
             groupMap: {}
         }
     },
@@ -29,12 +31,23 @@ export default {
     methods: {
         syncOrg(item) {
             console.log("开始同步组织机构");
+            this.loadOrgStatus = true;
             this.$API.device.cloudPlatform.service.syncOrg.post({deviceConnectorId: item.deviceConnectorId}).then(res => {
                 if(res.code !== '00000') {
                     this.$message.error(res.msg);
                     return;
                 }
-            })
+            }).finally(() => this.loadOrgStatus = false);
+        },
+        syncDevice(item) {
+            this.loadDeviceStatus = true;
+            console.log("开始同步设备");
+            this.$API.device.cloudPlatform.service.syncDevice.post({deviceConnectorId: item.deviceConnectorId}).then(res => {
+                if(res.code !== '00000') {
+                    this.$message.error(res.msg);
+                    return;
+                }
+            }).finally(() => this.loadDeviceStatus = false);
         },
     }
 }
