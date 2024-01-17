1e9217db657e26e376cbad1d70d0b695b4744680
ACHTK <yemengzhidiandig@gmail.com>
2024-01-17 21:28:27 +0800

    1

--- vue-support-admin-starter/src/views/setting/setting/sub/pushSettings.vue
+++ vue-support-admin-starter/src/views/setting/setting/sub/pushSettings.vue
@@ -3,10 +3,7 @@
 		<el-skeleton :loading="loading"></el-skeleton>
 		<el-form ref="form" label-width="180px" label-position="left" style="margin-top:20px;">
 			<el-form-item label="有新的待办">
-				<el-checkbox v-model="form.task_dx">短信推送</el-checkbox>
-				<el-checkbox v-model="form.task_wx">微信推送</el-checkbox>
-				<el-checkbox v-model="form.task_zn">站内推送</el-checkbox>
-				<el-checkbox v-model="form.task_yj">邮件推送</el-checkbox>
+				<el-checkbox v-model="item.settingValue" v-for="item in data">{{ item.settingDesc }}</el-checkbox>
 			</el-form-item>
 			<el-form-item>
 				<el-button type="primary" @click="save">保存</el-button>
@@ -20,32 +17,31 @@
 		data() {
 			return {
 				loading: true,
-				form: {
-					task_dx: false,
-					task_wx: false,
-					task_zn: false,
-					task_yj: false,
-				}
+				data: [{
+					settingName: 'task_dx',
+					settingDesc: '短信推送'
+				}, {
+					settingName: 'task_wx',
+					settingDesc: '微信推送'
+				}, {
+					settingName: 'task_zn',
+					settingDesc: '站内推送'
+				}, {
+					settingName: 'task_yj',
+					settingDesc: '邮件推送'
+				}]
 			}
 		},
 		mounted(){
 			this.$API.system.setting.list.get({keyword: 'task_*'}).then(res => {
 				if(res.code === '00000') {
-					res.data.forEach(element => {
-						this.form[element['settingName']] = element['settingValue'] == '1'
-					});
+					this.data = res.data;
 				}
 			}).finally(() => this.loading = false);
 		},
 		methods: {
 			save() {
-				const req = [];
-				req.push({settingName: 'task_dx', settingValue: this.form.task_dx ? '1' : '0'});
-				req.push({settingName: 'task_wx', settingValue: this.form.task_wx ? '1': '0'});
-				req.push({settingName: 'task_zn', settingValue: this.form.task_zn ? '1': '0'});
-				req.push({settingName: 'task_yj', settingValue: this.form.task_yj ? '1': '0'});
-
-				this.$API.system.setting.batch.post(req).then(res => {
+				this.$API.system.setting.batch.post(this.data).then(res => {
 					if(res.code === '00000') {
 						this.$message.success('保存成功');
 						return;
