2ca0f4994ddc941184460d60c936f4085da39718
ACHTK <yemengzhidiandig@gmail.com>
2024-01-27 18:28:02 +0800

    no commit message

---
+++ vue-support-leanring-starter/src/config/menu.js
@@ -0,0 +1,68 @@
+const me = [
+	{
+		path: "/home",
+		component: "home/index",
+		name: "首页",
+		hidden: false,
+		meta: {
+			title: "首页",
+			icon: "el-icon-eleme-filled",
+			hidden: null,
+			tag: null,
+			affix: false,
+			type: "menu",
+			color: null,
+			roles: [],
+			keepAlive: null,
+			params: null,
+		},
+		children: [
+			{
+				path: "/dashboard",
+				component: "home/index",
+				name: "控制台",
+				hidden: false,
+				meta: {
+					title: "控制台",
+					icon: "el-icon-menu",
+					hidden: null,
+					tag: null,
+					affix: false,
+					type: "menu",
+					color: null,
+					roles: [],
+					keepAlive: null,
+					params: null,
+				},
+			},
+		],
+	},
+	{
+		path: "/learning",
+		component: "learning/index",
+		name: "机器学习",
+		hidden: false,
+		meta: {
+			title: "机器学习",
+			icon: "sc-icon-scheduler",
+			type: "menu",
+		},
+		children: [
+			{
+				path: "/learning/detect",
+				component: "learning/detect/index",
+				name: "视觉检测",
+				hidden: false,
+				meta: {
+					title: "视觉检测",
+					icon: "el-icon-alarm-clock",
+					type: "menu",
+				},
+			},
+			
+			
+		],
+	},
+];
+
+export default me;
