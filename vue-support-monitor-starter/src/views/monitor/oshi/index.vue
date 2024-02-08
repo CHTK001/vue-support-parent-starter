a1b69e791312317fa49dd1d68b05789fc2439836
ACHTK <yemengzhidiandig@gmail.com>
2024-02-08 15:35:12 +0800

    1

--- vue-support-monitor-starter/src/views/monitor/oshi/index.vue
+++ vue-support-monitor-starter/src/views/monitor/oshi/index.vue
@@ -216,7 +216,6 @@ export default {
             this.form.appValue = this.$route.query.appName;
             const item = JSON.parse(Base64.decode(this.$route.query.data));
             this.form.appModelValue = item.serverHost + ':' + item.serverPort;
-            this.data = {};
         } catch (e) { }
         this.afterPrepertiesSet();
     },
