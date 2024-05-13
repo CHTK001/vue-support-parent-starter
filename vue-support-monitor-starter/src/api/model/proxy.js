c1a0277065305be162c62179a2be5833709c73db
Administrator <1@q>
2024-05-13 14:57:46 +0800

    1

---
+++ vue-support-monitor-starter/src/api/model/proxy.js
@@ -0,0 +1,54 @@
+import config from "@/config"
+import http from "@/utils/request"
+
+export default {
+    start: {
+        url: `${config.API_URL}/v1/proxy/start`,
+        name: "启动服务",
+        get: async function(params){
+            return await http.get(this.url, params);
+        }
+    },
+    stop: {
+        url: `${config.API_URL}/v1/proxy/stop`,
+        name: "暂停服务",
+        get: async function(params){
+            return await http.get(this.url, params);
+        }
+    },
+    list: {
+        url: `${config.API_URL}/v1/proxy/list`,
+        name: "查询服务",
+        get: async function(params){
+            return await http.get(this.url, params);
+        }
+    },
+    page: {
+        url: `${config.API_URL}/v1/proxy/page`,
+        name: "查询服务",
+        get: async function(params){
+            return await http.get(this.url, params);
+        }
+    },
+    save: {
+        url: `${config.API_URL}/v1/proxy/save`,
+        name: "添加服务",
+        post: async function(params){
+            return await http.post(this.url, params);
+        }
+    },
+    update: {
+        url: `${config.API_URL}/v1/proxy/update`,
+        name: "修改服务",
+        put: async function(params){
+            return await http.put(this.url, params);
+        }
+    },
+    delete: {
+        url: `${config.API_URL}/v1/proxy/delete`,
+        name: "删除服务",
+        delete: async function(params){
+            return await http.delete(this.url + `?id=${params.id}`, params);
+        }
+    },
+}
---
+++ vue-support-monitor-starter/src/views/proxy/info/save.vue
@@ -0,0 +1,126 @@
+<template>
+	<el-dialog :title="title" v-model="visible" :width="700"  destroy-on-close @closed="$emit('closed')" draggable>
+		<el-form :model="form" :rules="rules" :disabled="mode == 'show'" ref="dialogForm" label-width="100px" label-position="left">
+
+
+			<el-form-item label="代理名称" prop="proxyName">
+				<el-input v-model="form.proxyName" clearable placeholder="请输入代理名称"></el-input>
+			</el-form-item>
+
+			<el-form-item label="代理地址" prop="proxy">
+				<el-row>
+					<el-col :span="12">
+						<el-input v-model="form.proxyHost" placeholder="请输入代理地址"></el-input>
+					</el-col>
+					<el-col :span="12">
+						<el-input v-model="form.proxyPort" type="number" placeholder="请输入代理端口"></el-input>
+					</el-col>
+				</el-row>
+			</el-form-item>
+
+			<el-form-item label="协议" prop="proxyProtocol">
+				<el-select v-model="form.proxyProtocol" placeholder="请选择协议">
+					<el-option label="http" value="http"></el-option>
+					<el-option label="websocket" value="ws"></el-option>
+				</el-select>
+			</el-form-item>
+
+			<el-form-item label="说明" prop="proxyDesc">
+				<el-input v-model="form.proxyDesc" clearable placeholder="请输入说明"></el-input>
+			</el-form-item>
+
+		</el-form>
+		<template #footer>
+			<el-button @click="visible = false">取 消</el-button>
+			<el-button v-if="mode != 'show'" type="primary" :loading="isSaveing" @click="submit()">保 存</el-button>
+		</template>
+	</el-dialog>
+</template>
+
+<script>
+import pinyin from 'js-pinyin'
+
+export default {
+	emits: ['success', 'closed'],
+	data() {
+		return {
+			visible: false,
+			isSaveing: false,
+			configList: [],
+			title: '',
+			mode: '',
+			//表单数据
+			form: {
+				proxyHost: '127.0.0.1',
+				proxyProtocol: 'http'
+			},
+			//验证规则
+			rules: {
+				proxyName: [
+					{ required: true, message: '请输入代理名称', trigger: 'blur' }
+				],
+				proxyPort: [
+					{ required: true, message: '请输入说明', trigger: 'blur' }
+				],
+				proxyProtocol: [
+					{ required: true, message: '请选择协议', trigger: 'blur' }
+				]
+			},
+		}
+	},
+	mounted() {
+	},
+	methods: {
+		//显示
+		open(mode = 'add') {
+			this.mode = mode;
+			if(mode == 'add') {
+				this.title = '新增';
+			}
+			this.visible = true;
+			return this
+		},
+		//表单提交方法
+		submit() {
+			this.$refs.dialogForm.validate(async (valid) => {
+				if (valid) {
+					var res;
+					this.isSaveing = true;
+					if (this.mode === 'add') {
+						res = await this.$API.proxy.save.post(this.form);
+					} else if (this.mode === 'edit') {
+						res = await this.$API.proxy.update.put(this.form);
+					}
+					
+
+					this.isSaveing = false;
+					if (res.code == '00000') {
+						this.$emit('success', res, this.mode)
+						this.visible = false;
+					} else {
+						this.$message.error(res.msg)
+					}
+				}
+			})
+		},
+		//表单注入数据
+		setData(data) {
+			//可以和上面一样单个注入，也可以像下面一样直接合并进去
+			this.isSaveing = false;
+			Object.assign(this.form, data);
+			if(this.mode == 'edit') {
+				this.title = '修改' + this.form.proxyName;
+				return;
+			}
+			
+			if(this.mode == 'add') {
+				this.form = {};
+				this.form.proxyHost = '127.0.0.1';
+				this.form.proxyProtocol = 'http';
+			}
+		}
+	}
+}
+</script>
+
+<style></style>
