9f538e733ab21ba0e31e67d6a351ebfbe783eaa8
yemen <1@qq.com>
2024-08-17 12:16:41 +0800

    1

--- vue-support-system-parent/src/api/dept.ts
+++ vue-support-system-parent/src/api/dept.ts
@@ -1,11 +1,10 @@
 import { http } from "@/utils/http";
 
-
 /**
  * 删除组织机构
  */
 export const deleteDept = deptId => {
-    return http.request<Boolean>("delete", "/v2/dept/delete", {
-      data: { deptId: deptId }
-    });
-  };
+  return http.request<Boolean>("delete", "/v2/dept/delete", {
+    data: { deptId: deptId }
+  });
+};
