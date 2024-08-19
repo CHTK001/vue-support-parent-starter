ba481db7a39a6ceef8214f11369a94ab4cb7971a
ACHTK <yemengzhidiandig@gmail.com>
2024-08-19 11:18:07 +0800

    菜單管理

--- vue-support-system-parent/src/views/manage/menu/index.vue
+++ vue-support-system-parent/src/views/manage/menu/index.vue
@@ -1,70 +1,201 @@
 <script setup lang="ts">
-import { useRouter } from "vue-router";
-import noAccess from "@/assets/status/403.svg?component";
+import { h, reactive, ref, nextTick, toRaw } from "vue";
 
-defineOptions({
-  name: "403"
+import SaveDialog from "./save.vue";
+import Delete from "@iconify-icons/ep/delete";
+import EditPen from "@iconify-icons/ep/edit-pen";
+import Refresh from "@iconify-icons/line-md/backup-restore";
+import Edit from "@iconify-icons/line-md/plus";
+
+import { fetchDeleteMenu, fetchListMenu, Menu } from "@/api/menu";
+import { message } from "@/utils/message";
+import { useI18n } from "vue-i18n";
+import { transformI18n } from "@/plugins/i18n";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+
+const { t } = useI18n();
+const form = reactive({});
+
+const visible = reactive({
+  save: false
 });
 
-const router = useRouter();
+const loading = reactive({
+  query: false
+});
+const formRef = ref();
+const table = ref(null);
+const saveDialog = ref(null);
+const resetForm = async formRef => {
+  formRef.resetFields();
+  onSearch();
+};
+
+const tableData: Menu[] = reactive([]);
+const onSearch = async () => {
+  loading.query = true;
+  tableData.length = 0;
+  fetchListMenu(form)
+    .then(res => {
+      const { data, code } = res;
+      tableData.push(...data);
+      return;
+    })
+    .catch(error => {
+      message(t("message.queryFailed"), { type: "error" });
+    })
+    .finally(() => {
+      loading.query = false;
+    });
+};
+
+onSearch();
+
+const saveDialogParams = reactive({
+  mode: "save"
+});
+
+const onDelete = async row => {
+  try {
+    const { code } = await fetchDeleteMenu(row.sysMenuId);
+    onSearch();
+    message(t("message.deleteSuccess"), { type: "success" });
+    return;
+  } catch (error) {}
+};
+
+const dialogOpen = async (item, mode) => {
+  visible.save = true;
+  await nextTick();
+  saveDialog.value.setTableData(tableData).setData(item).open(mode);
+};
+
+const dialogClose = async () => {
+  visible.save = false;
+};
 </script>
 
 <template>
-  <div class="flex justify-center items-center h-[640px]">
-    <noAccess />
-    <div class="ml-12">
-      <p
-        v-motion
-        class="font-medium text-4xl mb-4 dark:text-white"
-        :initial="{
-          opacity: 0,
-          y: 100
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 80
-          }
-        }"
-      >
-        403
-      </p>
-      <p
-        v-motion
-        class="mb-4 text-gray-500"
-        :initial="{
-          opacity: 0,
-          y: 100
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 120
-          }
-        }"
-      >
-        抱歉，你无权访问该页面23333333333333333
-      </p>
-      <el-button
-        v-motion
-        type="primary"
-        :initial="{
-          opacity: 0,
-          y: 100
-        }"
-        :enter="{
-          opacity: 1,
-          y: 0,
-          transition: {
-            delay: 160
-          }
-        }"
-        @click="router.push('/')"
-      >
-        返回首页
-      </el-button>
+  <div>
+    <SaveDialog
+      v-if="visible.save"
+      ref="saveDialog"
+      :mode="saveDialogParams.mode"
+      @success="onSearch"
+      @close="dialogClose"
+    />
+    <div class="main">
+      <el-container>
+        <el-header>
+          <div class="left-panel" />
+          <div class="right-panel">
+            <div class="right-panel-search">
+              <el-button
+                type="primary"
+                :icon="useRenderIcon('ri:search-line')"
+                :loading="loading.query"
+                @click="onSearch"
+              />
+              <el-button
+                :icon="useRenderIcon(Edit)"
+                @click="dialogOpen({}, 'save')"
+              />
+            </div>
+          </div>
+        </el-header>
+        <el-main class="nopadding">
+          <div class="h-full">
+            <el-skeleton v-if="loading.query" animated :count="6" />
+            <el-table
+              v-else
+              :data="tableData"
+              style="width: 100%; margin-bottom: 20px"
+              row-key="sysMenuId"
+              border
+            >
+              <el-table-column prop="sysMenuTitle" label="菜单名称">
+                <template #default="{ row }">
+                  <span class="inline-block mr-1">
+                    <component
+                      :is="useRenderIcon(toRaw(row.sysMenuIcon))"
+                      style="padding-top: 1px"
+                    />
+                  </span>
+                  <span>{{ transformI18n(row.sysMenuTitle) }}</span>
+                </template>
+              </el-table-column>
+              <el-table-column prop="sysMenuType" label="菜单类型">
+                <template #default="{ row }">
+                  <el-tag
+                    v-if="row.sysMenuType == 0"
+                    size="small"
+                    type="primary"
+                    effect="plain"
+                    class="inline-block mr-2 p-8"
+                    >菜单</el-tag
+                  >
+                </template>
+              </el-table-column>
+              <el-table-column prop="sysMenuPath" label="路由路径" />
+              <el-table-column prop="sysMenuComponent" label="组件路径" />
+              <el-table-column prop="sysMenuSort" label="排序" />
+              <el-table-column prop="sysMenuHidden" label="隐藏">
+                <template #default="{ row }">
+                  {{ row.sysMenuHidden ? "是" : "否" }}
+                </template>
+              </el-table-column>
+              <el-table-column label="操作">
+                <template #default="{ row }">
+                  <el-button
+                    size="small"
+                    plain
+                    link
+                    type="primary"
+                    :icon="useRenderIcon(EditPen)"
+                    @click="dialogOpen(row, 'edit')"
+                    >编辑</el-button
+                  >
+                  <el-button
+                    size="small"
+                    plain
+                    link
+                    type="primary"
+                    :icon="useRenderIcon(Edit)"
+                    @click="
+                      dialogOpen({ sysMenuPid: row.sysMenuId } as any, 'save')
+                    "
+                    >新增</el-button
+                  >
+                  <el-popconfirm title="确定删除吗？" @confirm="onDelete(row)">
+                    <template #reference>
+                      <el-button
+                        size="small"
+                        type="danger"
+                        plain
+                        link
+                        :icon="useRenderIcon(Delete)"
+                        >删除</el-button
+                      >
+                    </template>
+                  </el-popconfirm>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+        </el-main>
+      </el-container>
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+:deep(.el-dropdown-menu__item i) {
+  margin: 0;
+}
+
+.search-form {
+  :deep(.el-form-item) {
+    margin-bottom: 12px;
+  }
+}
+</style>
---
+++ vue-support-system-parent/src/views/manage/menu/save.vue
@@ -0,0 +1,502 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { fetchListMenu, fetchUpdateMenu, fetchSaveMenu } from "@/api/menu";
+
+import { $t } from "@/plugins/i18n";
+import ReCol from "@/components/ReCol";
+import { message } from "@/utils/message";
+import Segmented from "@/components/ReSegmented";
+import { transformI18n } from "@/plugins/i18n";
+import { IconSelect } from "@/components/ReIcon";
+import ReAnimateSelector from "@/components/ReAnimateSelector/index.vue";
+export default defineComponent({
+  components: { Segmented, IconSelect, ReCol, ReAnimateSelector },
+  data() {
+    return {
+      dynamicTags: [],
+      form: {
+        sysMenuId: "",
+        sysMenuPid: "",
+        sysMenuTitle: "",
+        sysMenuName: "",
+        sysMenuType: 0,
+        sysMenuPerm: "",
+        sysMenuPath: "",
+        sysMenuI18n: "",
+        sysMenuComponent: "",
+        sysMenuIcon: "",
+        sysMenuSort: "",
+        sysMenuRole: "",
+        sysMenuRedirect: "",
+        sysMenuLeaveTransition: "",
+        sysMenuEnterTransition: "",
+        sysMenuFrameSrc: "",
+        sysMenuActivePath: "",
+        sysMenuShowParent: "",
+        sysMenuKeepAlive: 0,
+        sysMenuHiddenTag: 0,
+        sysMenuFixedTag: 0,
+        sysMenuHidden: ""
+      },
+      visible: false,
+      rules: {},
+      loading: false,
+      title: "",
+      mode: "save",
+      showParentOptions: [
+        {
+          label: "显示",
+          tip: "会显示父级菜单",
+          value: true
+        },
+        {
+          label: "隐藏",
+          tip: "不会显示父级菜单",
+          value: false
+        }
+      ],
+      hiddenTagOptions: [
+        {
+          label: "允许",
+          tip: "当前菜单名称或自定义信息允许添加到标签页",
+          value: false
+        },
+        {
+          label: "禁止",
+          tip: "当前菜单名称或自定义信息禁止添加到标签页",
+          value: true
+        }
+      ],
+      menuTypeOptions: [
+        {
+          label: "菜单",
+          value: 0
+        },
+        {
+          label: "iframe",
+          value: 1
+        },
+        {
+          label: "外链",
+          value: 2
+        },
+        {
+          label: "按钮",
+          value: 3
+        }
+      ],
+      keepAliveOptions: [
+        {
+          label: "缓存",
+          tip: "会保存该页面的整体状态，刷新后会清空状态",
+          value: true
+        },
+        {
+          label: "不缓存",
+          tip: "不会保存该页面的整体状态",
+          value: false
+        }
+      ],
+      fixedTagOptions: [
+        {
+          label: "固定",
+          tip: "当前菜单名称固定显示在标签页且不可关闭",
+          value: true
+        },
+        {
+          label: "不固定",
+          tip: "当前菜单名称不固定显示在标签页且可关闭",
+          value: false
+        }
+      ],
+      frameLoadingOptions: [
+        {
+          label: "开启",
+          tip: "有首次加载动画",
+          value: true
+        },
+        {
+          label: "关闭",
+          tip: "无首次加载动画",
+          value: false
+        }
+      ],
+      showLinkOptions: [
+        {
+          label: "显示",
+          tip: "会在菜单中显示",
+          value: true
+        },
+        {
+          label: "隐藏",
+          tip: "不会在菜单中显示",
+          value: false
+        }
+      ],
+      tableData: [],
+      props: {
+        value: "sysMenuId",
+        label: "sysMenuTitle",
+        emitPath: false
+      },
+      inputValue: "",
+      inputVisible: false
+    };
+  },
+  methods: {
+    async close() {
+      this.visible = false;
+      this.loading = false;
+      this.tableData.length = 0;
+      this.$nextTick(() => {
+        this.$refs?.dialogForm.resetFields();
+      });
+    },
+    handleClose(tag: string) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
+    setTableData(data) {
+      console.log(data);
+      Object.assign(this.tableData, data);
+      return this;
+    },
+    handleInputConfirm() {
+      if (this.inputValue) {
+        this.dynamicTags.push(this.inputValue);
+      }
+      this.inputVisible = false;
+      this.inputValue = "";
+    },
+    showInput() {
+      this.inputVisible = !0;
+      this.$nextTick(() => {
+        this.$refs.InputRef.input!.focus();
+      });
+    },
+    setData(data) {
+      Object.assign(this.form, data);
+      return this;
+    },
+    async open(mode = "save") {
+      this.visible = true;
+      this.mode = mode;
+      this.title = mode == "save" ? "新增" : "编辑";
+    },
+    transformI18nValue(value) {
+      return transformI18n(value);
+    },
+    submit() {
+      this.$refs.dialogForm.validate(async valid => {
+        if (valid) {
+          this.loading = true;
+          var res: any = {};
+          if (this.mode === "save") {
+            res = await fetchSaveMenu(this.form);
+          } else if (this.mode === "edit") {
+            res = await fetchUpdateMenu(this.form);
+          }
+
+          this.loading = false;
+          if (res.code == "00000") {
+            this.$emit("success");
+            this.visible = false;
+          } else {
+            message(res.msg, { type: "error" });
+          }
+        }
+      });
+    }
+  }
+});
+</script>
+<template>
+  <div>
+    <el-dialog
+      v-model="visible"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      draggable
+      :title="title"
+      @close="close"
+    >
+      <el-form
+        ref="dialogForm"
+        :model="form"
+        :rules="rules"
+        :disabled="mode == 'show'"
+        label-width="100px"
+      >
+        <el-row :gutter="30">
+          <re-col>
+            <el-form-item label="菜单类型">
+              <Segmented
+                v-model="form.sysMenuType"
+                :options="menuTypeOptions"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col>
+            <el-form-item label="上级菜单">
+              <el-cascader
+                v-model="form.sysMenuPid"
+                class="w-full"
+                :options="tableData"
+                :props="props"
+                clearable
+                filterable
+                placeholder="请选择上级菜单"
+              >
+                <template #default="{ node, data }">
+                  <span v-if="data.sysMenuI18n">{{
+                    transformI18nValue(data.sysMenuI18n)
+                  }}</span>
+                  <span v-else>{{ data.sysMenuTitle }}</span>
+                  <span v-if="!node.isLeaf">
+                    ({{ data.children.length }})
+                  </span>
+                </template>
+              </el-cascader>
+            </el-form-item>
+          </re-col>
+
+          <re-col :value="12" :xs="24" :sm="24">
+            <el-form-item label="菜单名称" prop="sysMenuTitle">
+              <el-input
+                v-model="form.sysMenuTitle"
+                clearable
+                placeholder="请输入菜单名称"
+              />
+            </el-form-item>
+          </re-col>
+          <re-col v-if="form.sysMenuType !== 3" :value="12" :xs="24" :sm="24">
+            <el-form-item label="路由名称" prop="sysMenuName">
+              <el-input
+                v-model="form.sysMenuName"
+                clearable
+                placeholder="请输入路由名称"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-if="form.sysMenuType !== 3" :value="12" :xs="24" :sm="24">
+            <el-form-item label="路由路径" prop="path">
+              <el-input
+                v-model="form.sysMenuPath"
+                clearable
+                placeholder="请输入路由路径"
+              />
+            </el-form-item>
+          </re-col>
+          <re-col v-show="form.sysMenuType === 0" :value="12" :xs="24" :sm="24">
+            <el-form-item label="组件路径">
+              <el-input
+                v-model="form.sysMenuComponent"
+                clearable
+                placeholder="请输入组件路径"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col :value="12" :xs="24" :sm="24">
+            <el-form-item label="菜单排序">
+              <el-input-number
+                v-model="form.sysMenuSort"
+                class="!w-full"
+                :min="1"
+                :max="9999"
+                controls-position="right"
+              />
+            </el-form-item>
+          </re-col>
+          <re-col v-show="form.sysMenuType === 0" :value="12" :xs="24" :sm="24">
+            <el-form-item label="路由重定向">
+              <el-input
+                v-model="form.sysMenuRedirect"
+                clearable
+                placeholder="请输入默认跳转地址"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType !== 3" :value="12" :xs="24" :sm="24">
+            <el-form-item label="菜单图标">
+              <IconSelect v-model="form.sysMenuIcon" class="w-full" />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType !== 3" :value="12" :xs="24" :sm="24">
+            <el-form-item label="右侧图标">
+              <el-input
+                v-model="form.sysMenuRedirect"
+                clearable
+                placeholder="菜单名称右侧的额外图标"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType < 2" :value="12" :xs="24" :sm="24">
+            <el-form-item label="进场动画">
+              <ReAnimateSelector
+                v-model="form.sysMenuEnterTransition"
+                placeholder="请选择页面进场加载动画"
+              />
+            </el-form-item>
+          </re-col>
+          <re-col v-show="form.sysMenuType < 2" :value="12" :xs="24" :sm="24">
+            <el-form-item label="离场动画">
+              <ReAnimateSelector
+                v-model="form.sysMenuLeaveTransition"
+                placeholder="请选择页面离场加载动画"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType === 0" :value="12" :xs="24" :sm="24">
+            <el-form-item label="菜单激活">
+              <el-input
+                v-model="form.sysMenuActivePath"
+                clearable
+                placeholder="请输入需要激活的菜单"
+              />
+            </el-form-item>
+          </re-col>
+          <re-col v-if="form.sysMenuType === 3" :value="12" :xs="24" :sm="24">
+            <!-- 按钮级别权限设置 -->
+            <el-form-item label="权限标识" prop="sysMenuPerm">
+              <el-input
+                v-model="form.sysMenuPerm"
+                clearable
+                placeholder="请输入权限标识"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType === 1" :value="12" :xs="24" :sm="24">
+            <!-- iframe -->
+            <el-form-item label="链接地址">
+              <el-input
+                v-model="form.sysMenuFrameSrc"
+                clearable
+                placeholder="请输入 iframe 链接地址"
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType !== 3" :value="12" :xs="24" :sm="24">
+            <el-form-item label="菜单">
+              <Segmented
+                :modelValue="form.sysMenuHidden ? 0 : 1"
+                :options="showLinkOptions"
+                @change="
+                  ({ option: { value } }) => {
+                    form.sysMenuHidden = value;
+                  }
+                "
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType !== 3" :value="12" :xs="24" :sm="24">
+            <el-form-item label="父级菜单">
+              <Segmented
+                :modelValue="form.sysMenuShowParent ? 0 : 1"
+                :options="showParentOptions"
+                @change="
+                  ({ option: { value } }) => {
+                    form.sysMenuShowParent = value;
+                  }
+                "
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType < 2" :value="12" :xs="24" :sm="24">
+            <el-form-item label="缓存页面">
+              <Segmented
+                :modelValue="form.sysMenuKeepAlive ? 0 : 1"
+                :options="keepAliveOptions"
+                @change="
+                  ({ option: { value } }) => {
+                    form.sysMenuKeepAlive = value;
+                  }
+                "
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType < 2" :value="12" :xs="24" :sm="24">
+            <el-form-item label="标签页">
+              <Segmented
+                :modelValue="form.sysMenuHiddenTag ? 1 : 0"
+                :options="hiddenTagOptions"
+                @change="
+                  ({ option: { value } }) => {
+                    form.sysMenuHiddenTag = value;
+                  }
+                "
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col v-show="form.sysMenuType < 2" :value="12" :xs="24" :sm="24">
+            <el-form-item label="固定标签页">
+              <Segmented
+                :modelValue="form.sysMenuFixedTag ? 0 : 1"
+                :options="fixedTagOptions"
+                @change="
+                  ({ option: { value } }) => {
+                    form.sysMenuFixedTag = value;
+                  }
+                "
+              />
+            </el-form-item>
+          </re-col>
+
+          <re-col :value="12" :xs="24" :sm="24">
+            <el-form-item label="所属权限">
+              <el-tag
+                v-for="tag in dynamicTags"
+                :key="tag"
+                closable
+                style="margin: 5px"
+                :disable-transitions="false"
+                @close="handleClose(tag)"
+              >
+                {{ tag }}
+              </el-tag>
+              <el-input
+                v-if="inputVisible"
+                ref="InputRef"
+                v-model="inputValue"
+                class="w-20"
+                size="small"
+                @keyup.enter="handleInputConfirm"
+                @blur="handleInputConfirm"
+              />
+              <el-button v-else class="button-new-tag" @click="showInput">
+                + {{ transformI18nValue("message.auth.tag") }}
+              </el-button>
+            </el-form-item>
+          </re-col>
+        </el-row>
+      </el-form>
+
+      <template #footer>
+        <el-button @click="visible = false">取 消</el-button>
+        <el-button
+          v-if="mode != 'show'"
+          type="primary"
+          :loading="loading"
+          @click="submit()"
+          >保 存</el-button
+        >
+      </template>
+    </el-dialog>
+  </div>
+</template>
+<style scoped>
+.el-cascader-panel .el-radio {
+  display: none;
+}
+</style>
