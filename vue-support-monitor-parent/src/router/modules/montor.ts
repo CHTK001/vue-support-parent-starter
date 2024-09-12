93586ce389cd193a00d7ec859b289394596f3ca0
ACHTK <yemengzhidiandig@gmail.com>
2024-09-12 16:47:45 +0800

    1

--- vue-support-monitor-parent/src/router/modules/montor.ts
+++ vue-support-monitor-parent/src/router/modules/montor.ts
@@ -15,8 +15,29 @@ export default [
         name: "service",
         component: () => import("@/views/monitor/service/index.vue"),
         meta: {
-          icon: "ep:service",
+          icon: "ri:navigation-fill",
           title: $t("buttons.monitor.service-list"),
+          showLink: true
+        }
+      }
+    ]
+  },
+  {
+    path: "/job",
+    name: "job",
+    meta: {
+      icon: "line-md:bell-twotone-loop",
+      title: $t("buttons.monitor.job"),
+      showLink: true
+    },
+    children: [
+      {
+        path: "/service",
+        name: "service",
+        component: () => import("@/views/monitor/job/index.vue"),
+        meta: {
+          icon: "line-md:bell-twotone-loop",
+          title: $t("buttons.monitor.job-list"),
           showLink: true
         }
       }
