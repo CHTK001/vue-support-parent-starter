df7b4cbc40e573ff096ec12d00f2ab799f177368
ACHTK <yemengzhidiandig@gmail.com>
2024-09-12 16:28:42 +0800

    1

---
+++ vue-support-monitor-parent/src/views/manage/setting/save2.vue
@@ -0,0 +1,119 @@
+<script>
+import { defineComponent } from "vue";
+import { fetchSetting, fetchUpdateSetting, fetchSaveSetting } from "@/api/setting";
+import { transformI18n } from "@/plugins/i18n";
+import Save from "@iconify-icons/ep/refresh";
+import { message } from "@/utils/message";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+export default defineComponent({
+  data() {
+    return {
+      form: {},
+      valueType: [
+        { value: "string", label: "字符串" },
+        { value: "number", label: "数字" },
+        { value: "bool", label: "布尔" },
+        { value: "array", label: "数组" },
+        { value: "object", label: "对象" }
+      ],
+      visible: false,
+      rules: {
+        sysSettingName: [{ required: true, message: "请输入配置名称", trigger: "blur" }],
+        sysSettingValue: [{ required: true, message: "请输入配置值", trigger: "blur" }],
+        sysSettingValueType: [{ required: true, message: "请输入配置值类型", trigger: "blur" }],
+        sysSettingGroup: [{ required: true, message: "请输入配置所属分组", trigger: "blur" }]
+      },
+      loading: false,
+      layoutLoading: false,
+      title: "",
+      Save: null,
+      mode: "save",
+      groupList: []
+    };
+  },
+  mounted() {
+    this.Save = useRenderIcon(Save);
+  },
+  methods: {
+    async close() {
+      this.visible = false;
+      this.loading = false;
+      this.layoutLoading = false;
+      this.groupList.length = 0;
+      this.$emit("close");
+    },
+    transformI18nValue(val) {
+      return transformI18n(val);
+    },
+    setData(data) {
+      this.layoutLoading = true;
+      Object.assign(this.form, data);
+      fetchSetting(data.group)
+        .then(res => {
+          this.groupList.push(...res?.data);
+        })
+        .finally(() => {
+          this.layoutLoading = false;
+        });
+      return this;
+    },
+    async open(mode = "save") {
+      this.visible = true;
+      this.mode = mode;
+      this.title = this.form.name;
+    },
+    async submit(item) {
+      this.loading = true;
+      var res = {};
+      if (this.mode === "save") {
+        res = await fetchSaveSetting(item);
+      } else if (this.mode === "edit") {
+        res = await fetchUpdateSetting(item);
+      }
+
+      if (res.code == "00000") {
+        this.$emit("success", item, this.mode);
+      } else {
+        message(res.msg, { type: "error" });
+      }
+      this.loading = false;
+    }
+  }
+});
+</script>
+<template>
+  <div>
+    <el-drawer v-model="visible" size="30%" :close-on-click-modal="false" :close-on-press-escape="false" draggable :title="title" @close="close">
+      <div v-if="!layoutLoading">
+        <el-empty v-if="!groupList || groupList.length == 0" />
+        <ul v-else class="setting">
+          <li v-for="(item, $index) in groupList" :key="$index" :title="item.sysSettingRemark">
+            <div style="font-size: 15px; line-height: 15px">{{ item.sysSettingRemark || item.sysSettingName }}</div>
+            <el-form :inline="true">
+              <el-form-item>
+                <el-switch v-if="item.sysSettingValueType == 'bool'" v-model="item.sysSettingValue" active-value="true" inactive-value="false" inline-prompt @change="submit(item)" />
+                <el-input-number v-else-if="item.sysSettingValueType == 'number'" v-model="item.sysSettingValue" inline-prompt />
+                <el-input v-else v-model="item.sysSettingValue" inline-prompt />
+              </el-form-item>
+              <el-form-item v-if="item.sysSettingValueType !== 'bool'">
+                <el-button class="ml-1" :icon="Save" @click="submit(item)" />
+              </el-form-item>
+            </el-form>
+          </li>
+        </ul>
+      </div>
+      <el-skeleton v-else animated />
+    </el-drawer>
+  </div>
+</template>
+<style lang="scss" scoped>
+.setting {
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 3px 0;
+    font-size: 14px;
+  }
+}
+</style>
--- vue-support-monitor-parent/src/views/monitor/service/index.vue
+++ vue-support-monitor-parent/src/views/monitor/service/index.vue
@@ -1,15 +1,58 @@
 <template>
   <div>
-    <ScCard :data="data.tableData" :params="params">
-      <div>1</div>
-    </ScCard>
+    <div class="h-[100px] p-4">
+      <div class="flex flex-[2] justify-end">
+        <div class="w-[300px] flex">
+          <el-select v-model="countDownTime" class="w-[100px] basis-2/4">
+            <el-option :value="5" label="5s" />
+            <el-option :value="10" label="10s" />
+            <el-option :value="20" label="20s" />
+            <el-option :value="30" label="30s" />
+            <el-option :value="60" label="60" />
+          </el-select>
+          <ScCountDown v-model="countDownTime" :loop="true" class="block basis-2/4 pl-2 mt-[3px]" @finish="refresh">
+            <template #default="{ row }">
+              <span>{{ row.seconds }}s 后刷新数据</span>
+            </template>
+          </ScCountDown>
+        </div>
+      </div>
+    </div>
+    <div class="h-full pl-4 pr-4">
+      <ScCard :data="data.tableData" :params="params">
+        <template #default="{ row }">
+          <div clas="flex flex-1">
+            <el-descriptions border :column="2" class="w-full" :title="row.metadata?.applicationName">
+              <template #title>
+                <span>
+                  {{ row.metadata?.applicationName }}
+                  <el-icon><component :is="useRenderIcon('humbleicons:activity')" /></el-icon>
+                </span>
+              </template>
+              <el-descriptions-item label="应用">{{ row.metadata?.applicationName }}</el-descriptions-item>
+              <el-descriptions-item label="地址">{{ row.metadata?.applicationHost }}:{{ row.metadata?.applicationPort }}</el-descriptions-item>
+              <el-descriptions-item label="环境">
+                {{ row.metadata?.applicationActive }}
+              </el-descriptions-item>
+              <el-descriptions-item label="配置">
+                <el-tooltip :content="row.metadata?.applicationActiveInclude" placement="top" effect="dark">
+                  <el-icon><component :is="useRenderIcon('ri:settings-4-line')" /></el-icon>
+                </el-tooltip>
+              </el-descriptions-item>
+            </el-descriptions>
+          </div>
+        </template>
+      </ScCard>
+    </div>
   </div>
 </template>
 <script setup>
 import { fetchServiceList } from "@/api/monitor/service";
 import ScTable from "@/components/scTable/index.vue";
 import ScCard from "@/components/scCard/index.vue";
-import { markRaw, reactive } from "vue";
+import { markRaw, onMounted, reactive, ref } from "vue";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+import ScCountDown from "@/components/ScCountDown/index.vue";
 
 const params = reactive({
   page: 1,
@@ -20,10 +63,32 @@ const params = reactive({
 const data = reactive({
   tableData: markRaw([])
 });
+
+const countDownTime = ref(10);
 const getData = () => {
   data.tableData.length = 0;
   fetchServiceList(params).then(res => {
     data.tableData = res.data;
   });
 };
+const refresh = () => {
+  fetchServiceList(params).then(res => {
+    data.tableData = res.data;
+  });
+};
+
+onMounted(async () => {
+  getData();
+});
 </script>
+<style lang="scss" scoped>
+:deep(.el-descriptions__body .el-descriptions__table) {
+  white-space: nowrap;
+}
+:deep(.el-descriptions-item__content) {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-right: 8px;
+}
+</style>
