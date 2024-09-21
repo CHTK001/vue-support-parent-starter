8e22731fd53b35be49ffa0038d184d714993e749
yemen <1@qq.com>
2024-09-21 20:57:46 +0800

    1

--- vue-support-monitor-parent/src/views/monitor/service/dashboard/portlet/process.vue
+++ vue-support-monitor-parent/src/views/monitor/service/dashboard/portlet/process.vue
@@ -43,7 +43,39 @@ const diskOptions = reactive({
       formatter: function (param) {
         return param.name;
       }
-    }
+    },
+    levels: [
+      {},
+      {
+        r0: "15%",
+        r: "35%",
+        itemStyle: {
+          borderWidth: 2
+        },
+        label: {
+          rotate: "tangential"
+        }
+      },
+      {
+        r0: "35%",
+        r: "70%",
+        label: {
+          align: "right"
+        }
+      },
+      {
+        r0: "70%",
+        r: "72%",
+        label: {
+          position: "outside",
+          padding: 3,
+          silent: false
+        },
+        itemStyle: {
+          borderWidth: 3
+        }
+      }
+    ]
   }
 });
 
