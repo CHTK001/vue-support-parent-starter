ab818009bf1003a72eb6fe4764f0b1cbdcbd4edc
ACHTK <yemengzhidiandig@gmail.com>
2024-11-13 16:59:23 +0800

    feat(config): 修改平台标题为代理管理
    - 将平台标题从"后台管理系统"修改为"代理管理"
    - 此修改提升了界面的准确性，使标题与系统功能更加匹配

--- vue-support-hotspot-parent/src/config/setting.ts
+++ vue-support-hotspot-parent/src/config/setting.ts
@@ -14,7 +14,7 @@ export const globalSetting: PlatformConfigs = {
   storageKey: "2XNN4K8LC0ELVWN4", // 缓存加密密钥
   baseUrl: "",
   Version: "0.7.0", // 平台版本号
-  Title: "后台管理系统", // 平台标题
+  Title: "代理管理", // 平台标题
   FixedHeader: true, // 是否固定页头和标签页（true 内容区超出出现纵向滚动条 false 页头、标签页、内容区可纵向滚动）
   HiddenSideBar: false, // 隐藏菜单和页头，只显示标签页和内容区
   MultiTagsCache: false, // 是否开启持久化标签 （会缓存）
