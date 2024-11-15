d7f9dc7724ec033db83e609164513faf9143892c
ACHTK <yemengzhidiandig@gmail.com>
2024-11-15 14:10:46 +0800

    1

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -4,12 +4,10 @@ export * from "./objects";
 export * from "./base64";
 export * from './sfc';
 export * from './tree';
-export * from './url';
 export * from './letter';
 export * from './date';
 export * from './chinaArea';
 export * from './message';
 export * from './progress/index'
 export * from './localforage/index'
-export * from './localforage/types'
 export default NProgress;
