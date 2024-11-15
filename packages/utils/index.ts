d86413019c3429eecdf856aa4ee22adfcb26b7d5
ACHTK <yemengzhidiandig@gmail.com>
2024-11-15 14:52:44 +0800

    1

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,13 +1,13 @@
 import NProgress from "nprogress";
 
-export * from "./src/objects";
-export * from "./src/base64";
-export * from './src/sfc';
-export * from './src/tree';
-export * from './src/letter';
-export * from './src/date';
-export * from './src/chinaArea';
-export * from './src/message';
+export * from "./src/util/objects";
+export * from "./src/util/base64";
+export * from './src/util/sfc';
+export * from './src/util/tree';
+export * from './src/util/letter';
+export * from './src/util/date';
+export * from './src/util/chinaArea';
+export * from './src/util/message';
 export * from './src/progress/index'
 export * from './src/repo-localforage/index'
 export default NProgress;
