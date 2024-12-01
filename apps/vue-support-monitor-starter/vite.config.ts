45b09f9704e666150e886084b3d4bf2d4ba2dafb
ACHTK <yemengzhidiandig@gmail.com>
2024-12-01 18:52:00 +0800

    build(vue-support-monitor-starter): 调整项目配置以提高代码质量和兼容性
    - 在 tsconfig.json 中添加 noWarnings 和 warningsAsErrors 选项，提高 TypeScript编译的严格性
    - 修改 vite.config.ts 中的 build.target为 esnext，以生成更现代的 JavaScript 代码

--- apps/vue-support-monitor-starter/vite.config.ts
+++ apps/vue-support-monitor-starter/vite.config.ts
@@ -35,7 +35,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
     },
     build: {
       // https://cn.vitejs.dev/guide/build.html#browser-compatibility
-      target: "es2015",
+      target: "esnext",
       sourcemap: false,
       // 消除打包大小超过500kb警告
       chunkSizeWarningLimit: 4000,
