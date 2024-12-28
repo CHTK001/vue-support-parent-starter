22c7231383d8f3caa7292e8498aaf4f5c8a3e522
yemen <1@qq.com>
2024-12-28 13:07:29 +0800

    refactor(components): 重构验证码组件
    - 更新了 ScCode 组件的实现，使用了更简洁的模板和脚本结构
    - 优化了验证码输入的逻辑，支持自动跳转到下一个输入框
    - 添加了处理粘贴事件的功能，支持一次性粘贴多个字符
    - 调整了组件样式，使其更加符合现代设计

--- packages/components/ScCode/index.vue
+++ packages/components/ScCode/index.vue
@@ -1,160 +1,93 @@
 <template>
-	<view class="yi-code" :style="'width: '+width+'px;'">
-		<view class="yi-code-show" :style="'width: '+width+'px;'">
-			<block v-for="i in numberArr">
-				<view
-					:class="'yi-code-show-item' + ((codes.length === i && isFocus) ? ' yi-code-show-active' : '')" 
-					:style="type !== 'block' ? (type === 'line' ? 'border-top: 0; border-left: 0; border-right: 0; border-radius: 0; border-width: 2px;' : 'border-style: dashed;') : ''"
-				> 
-				{{showVal(codes[i])}}
-				</view>
-			</block>
-		</view>
-		<view class="yi-code-hide" :style="'width: '+(width * 2)+'px;left: -'+ width +'px;'">
-			<input v-model="value" :style="'width: '+(width * 2)+'px;'" :focus="focus" @focus="onFocus" @blur="onBlur" :type="inputType" @input="onChange" :maxlength="maxlength"/>
-		</view>
-	</view>
+  <div class="verification-container">
+    <input v-for="(code, index) in verificationCodes" :key="index" v-model="verificationCodes[index]" @input="handleInput(index, $event)" @keydown="handleKeyDown(index, $event)" maxlength="1" class="verification-input" />
+  </div>
 </template>
 
-<script>
-	export default {
-		props: {
-			/**
-			 * @description 宽度 px
-			 */
-			width: {
-				type: Number,
-				default: 300,
-			},
-			/**
-			 * @description 是否自动聚焦
-			 */
-			focus: {
-				type: Boolean,
-				default: true,
-			},
-			/**
-			 * @description 隐藏字符（密码效果）
-			 */
-			hide: {
-				type: String,
-				default: '',
-			},
-			/**
-			 * @description 验证码长度
-			 */
-			maxlength: {
-				type: Number,
-				default: 6
-			},
-			/**
-			 * @description 样式类型 block 方框, dashed 虚线方框，line 线
-			 */
-			type: {
-				type: String,
-				default: 'block'
-			},
-			/**
-			 * @description text | number 
-			 */
-			inputType: {
-				type: String,
-				default: 'number'
-			},
-		},
-		created() {
-			let arr = [];
-			for(let i = 0; i < this.$props.maxlength; i++){
-				arr.push(i);
-			}
-			this.numberArr = arr;
-		},
-		data(){
-			return {
-				numberArr: [], // 
-				codes: [],
-				value: '',
-				isFocus: false
-			}
-		},
-		methods: {
-			clear(){
-				this.codes = []
-				this.value = ''
-			},
-			onFocus(){
-				this.isFocus = true
-			},
-			onBlur(){
-				this.isFocus = false
-			},
-			showVal(v){
-				return v ? (this.hide || v) : '';
-			},
-			onChange(e){
-				let str = e.detail.value || e.target.value
-				if(str){
-					this.codes = String(str).split('');
-					this.$emit('onChange', str);
-					if(this.codes.length === this.$props.maxlength){
-						this.$emit('onComplete', str);
-					}
-				}else{
-					this.codes = [];
-					this.$emit('onChange', '');
-				}
-			}
-		}
-	}
+<script setup>
+import { ref, nextTick, defineEmits, defineExpose } from 'vue';
+const verificationCodes = ref(['', '', '', '', '', '']);
+
+const emit = defineEmits(['complete', 'change']);
+
+window.addEventListener('paste', e => {
+  const clipboardData = event.clipboardData || window.clipboardData;
+  const pastedText = clipboardData.getData('text');
+  let text = pastedText;
+  if (pastedText.length > 6) {
+    text = pastedText.substring(0, 6);
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    verificationCodes.value[i] = char;
+  }
+  const _value = verificationCodes.value.join("");
+  if (_value.length == 6) {
+    emit("onComplete", _value)
+  }
+})
+const handleInput = (index, event) => {
+  const value = event.target.value;
+  verificationCodes.value[index] = value;
+  const _value = verificationCodes.value.join("");
+  // 自动跳到下一个输入框
+  if (value && index < verificationCodes.value.length - 1) {
+    const nextInput = event.target.nextElementSibling;
+    if (nextInput) {
+      nextTick(() => {
+        emit("onChange", _value)
+        nextInput.focus();
+      });
+    }
+    return;
+  }
+  if (_value.length == 6) {
+    console.log("完成" + _value)
+    emit("onComplete", _value)
+
+  }
+
+};
+
+const clear = () => {
+  verificationCodes.value = ['', '', '', '', '', '']
+}
+const handleKeyDown = (index, event) => {
+  // 处理删除操作
+  if (event.key === 'Backspace' && !event.target.value && index > 0) {
+    const prevInput = event.target.previousElementSibling;
+    if (prevInput) {
+      nextTick(() => {
+        prevInput.focus();
+      });
+    }
+  }
+};
+
+defineExpose({
+  clear,
+});
 </script>
 
-<style lang="scss">
-	.yi-code {
-		position: relative;
-		overflow: hidden;
-		text-align: unset;
+<style lang="scss" scoped>
+.verification-container {
+  display: flex;
+}
 
-		.yi-code-show {
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
+.verification-input {
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+  text-align: center;
+  font-size: 18px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
 
-			.yi-code-show-item {
-				box-sizing: border-box;
-				width: 40px;
-				height: 40px;
-				border: 1px solid #777;
-				border-radius: 6px;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-			}
-
-			.yi-code-show-active {
-				border-color: #ff5500;
-				border-width: 2px;
-				animation: myfirst 600ms infinite;
-				@keyframes myfirst
-				{
-					0%   {opacity: 0.1}
-					100% {opacity: 1}
-				}
-			}
-		}
-		
-		.yi-code-hide{
-			position: absolute;
-			z-index: 99;
-			left: 0;
-			top: 0;
-			height: 40px;
-			opacity: 0;
-			text-align: unset;
-			
-			input{
-				height: 40px;
-				text-align: unset;
-			}
-		}
-	}
+.verification-input:focus {
+  outline: none;
+  border-color: #007bff;
+  box-shadow: 0 0 5px #007bff;
+}
 </style>
