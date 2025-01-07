9baae7372caa6cdac3bb3b9c3d9dcb509c0cbed0
yemen <1@qq.com>
2025-01-07 20:50:12 +0800

    refactor(monitor): 优化代码结构和布局
    - 调整页面布局，增加顶部栏高度
    - 优化表格和卡片视图的代码结构
    - 统一操作按钮样式
    - 调整标签和图标样式

--- apps/vue-support-monitor-starter/src/views/monitor/gen/index.vue
+++ apps/vue-support-monitor-starter/src/views/monitor/gen/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-4">
-    <div class="w-full flex justify-end">
+  <div class="p-1">
+    <div class="w-full h-[70px] flex justify-end">
       <el-button :icon="useRenderIcon('ri:add-fill')" @click="onSave({}, 'add')" />
       <el-select v-model="showMode" class="!w-[200px]">
         <el-option label="列表" value="LIST" />
@@ -14,122 +14,124 @@
         </template>
       </el-input>
     </div>
-    <ScTable v-if="showMode === 'LIST'" ref="tableRef" :url="fetchGenDatabasePage" :params="searchParams" border>
-      <el-table-column label="序号" type="index" width="100px" />
-      <el-table-column label="图标" max-width="500px" align="left" show-overflow-tooltip>
-        <template #default="{ row }">
-          <el-icon :size="24" :color="row.genType == 'SHELL' ? '#00a870' : ''">
-            <component :is="getIcon(row)" />
-          </el-icon>
-          <span class="relative pl-[4px]">{{ row.genName }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="数据库/服务器">
-        <template #default="{ row }">
-          <div class="flex">
-            <div v-if="row.genDatabase">
-              <el-tag :color="row?.genDatabase ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">{{ row.genDatabase }}</el-tag>
-            </div>
-            <div v-else-if="row?.genHost && row?.genPort && row.genDriverRemoteUrl && row.genDriverRemoteUrl?.indexOf('null') == -1">
-              <span class="text-[#3f3f3f] mx-1 list-card-item_detail--operation--tag">{{ row.genDriverRemoteUrl }}</span>
-            </div>
-            <div v-else>/</div>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column label="状态" width="100px">
-        <template #default="{ row }">
-          <el-tag :type="row.genStatus == 0 ? 'danger' : 'success'" :size="row.genStatus == 0 ? 'mini' : 'default'">
-            {{ row.genStatus == 0 ? "停用" : "启用" }}
-          </el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column label="支持模块">
-        <template #default="{ row }">
-          <div class="flex">
-            <div>
-              <el-tag :color="row?.supportBackup != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">备份</el-tag>
-            </div>
-            <div>
-              <el-tag :color="row?.supportDocument != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">文档</el-tag>
-            </div>
-            <div>
-              <el-tag :color="row?.supportDriver != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">驱动</el-tag>
-            </div>
-          </div>
-        </template>
-      </el-table-column>
-
-      <el-table-column label="操作" fixed="right">
-        <template #default="{ row }">
-          <el-button size="small" plain type="default" :icon="useRenderIcon('ep:management')" @click="handleClickManage(row)" />
-          <el-button size="small" plain type="default" :icon="useRenderIcon('ep:edit')" @click="handleClickEdit(row)" />
-          <el-button size="small" plain type="danger" :icon="useRenderIcon('ep:delete')" @click="handleClickDelete(row)" />
-        </template>
-      </el-table-column>
-    </ScTable>
-    <ScCard v-if="showMode === 'CARD'" ref="tableRef" :url="fetchGenDatabasePage" :params="searchParams" :span="4">
-      <template #default="{ row }">
-        <div :class="['list-card-item', { 'list-card-item__disabled': false }]">
-          <div class="list-card-item_detail bg-bg_color">
-            <div class="flex flex-1 justify-between">
-              <div :class="['list-card-item_detail--logo1', { 'list-card-item_detail--logo__disabled': row?.genBackupStatus == 0 && row.supportBackup }]">
-                <el-icon class="bg-transparent" size="40">
-                  <component :is="getIcon(row)" />
-                </el-icon>
+    <div style="height: calc(100% - 70px)">
+      <ScTable v-if="showMode === 'LIST'" ref="tableRef" :url="fetchGenDatabasePage" :params="searchParams" border>
+        <el-table-column label="序号" type="index" width="100px" />
+        <el-table-column label="图标" max-width="500px" align="left" show-overflow-tooltip>
+          <template #default="{ row }">
+            <el-icon :size="24" :color="row.genType == 'SHELL' ? '#00a870' : ''">
+              <component :is="getIcon(row)" />
+            </el-icon>
+            <span class="relative pl-[4px]">{{ row.genName }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="数据库/服务器">
+          <template #default="{ row }">
+            <div class="flex">
+              <div v-if="row.genDatabase">
+                <el-tag :color="row?.genDatabase ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">{{ row.genDatabase }}</el-tag>
               </div>
-              <div />
-              <div />
-              <div />
-              <div />
-              <el-tag v-if="row.isFileDriver">文件</el-tag>
-              <el-tag v-if="row.supportBackup" :color="row?.genBackupStatus != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">
-                {{ row?.genBackupStatus != 0 ? "备份启用" : "备份停用" }}
-              </el-tag>
+              <div v-else-if="row?.genHost && row?.genPort && row.genDriverRemoteUrl && row.genDriverRemoteUrl?.indexOf('null') == -1">
+                <span class="text-[#3f3f3f] mx-1 list-card-item_detail--operation--tag">{{ row.genDriverRemoteUrl }}</span>
+              </div>
+              <div v-else>/</div>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" width="100px">
+          <template #default="{ row }">
+            <el-tag :type="row.genStatus == 0 ? 'danger' : 'success'" :size="row.genStatus == 0 ? 'mini' : 'default'">
+              {{ row.genStatus == 0 ? "停用" : "启用" }}
+            </el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="支持模块">
+          <template #default="{ row }">
+            <div class="flex">
               <div>
-                <div v-if="row?.genBackupStatus >= 0">
-                  <el-dropdown trigger="click">
-                    <el-icon>
-                      <component :is="useRenderIcon('ri:more-2-fill')" class="text-[24px]" />
-                    </el-icon>
-                    <template #dropdown>
-                      <el-dropdown-menu>
-                        <el-dropdown-item @click="handleClickManage(row)">管理</el-dropdown-item>
-                        <el-dropdown-item @click="handleClickEdit(row)">编辑</el-dropdown-item>
-                        <el-dropdown-item @click="handleClickDelete(row)">删除</el-dropdown-item>
-                      </el-dropdown-menu>
-                    </template>
-                  </el-dropdown>
+                <el-tag :color="row?.supportBackup != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">备份</el-tag>
+              </div>
+              <div>
+                <el-tag :color="row?.supportDocument != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">文档</el-tag>
+              </div>
+              <div>
+                <el-tag :color="row?.supportDriver != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">驱动</el-tag>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="操作" fixed="right">
+          <template #default="{ row }">
+            <el-button size="small" plain type="default" :icon="useRenderIcon('ep:management')" @click="handleClickManage(row)" />
+            <el-button size="small" plain type="default" :icon="useRenderIcon('ep:edit')" @click="handleClickEdit(row)" />
+            <el-button size="small" plain type="danger" :icon="useRenderIcon('ep:delete')" @click="handleClickDelete(row)" />
+          </template>
+        </el-table-column>
+      </ScTable>
+      <ScCard v-if="showMode === 'CARD'" ref="tableRef" :url="fetchGenDatabasePage" :params="searchParams" :span="4">
+        <template #default="{ row }">
+          <div :class="['list-card-item', { 'list-card-item__disabled': false }]">
+            <div class="list-card-item_detail bg-bg_color">
+              <div class="flex flex-1 justify-between">
+                <div :class="['list-card-item_detail--logo1', { 'list-card-item_detail--logo__disabled': row?.genBackupStatus == 0 && row.supportBackup }]">
+                  <el-icon class="bg-transparent" size="40">
+                    <component :is="getIcon(row)" />
+                  </el-icon>
+                </div>
+                <div />
+                <div />
+                <div />
+                <div />
+                <el-tag v-if="row.isFileDriver">文件</el-tag>
+                <el-tag v-if="row.supportBackup" :color="row?.genBackupStatus != 0 ? '#00a870' : '#ccc'" effect="dark" class="mx-1 list-card-item_detail--operation--tag">
+                  {{ row?.genBackupStatus != 0 ? "备份启用" : "备份停用" }}
+                </el-tag>
+                <div>
+                  <div v-if="row?.genBackupStatus >= 0">
+                    <el-dropdown trigger="click">
+                      <el-icon>
+                        <component :is="useRenderIcon('ri:more-2-fill')" class="text-[24px]" />
+                      </el-icon>
+                      <template #dropdown>
+                        <el-dropdown-menu>
+                          <el-dropdown-item @click="handleClickManage(row)">管理</el-dropdown-item>
+                          <el-dropdown-item @click="handleClickEdit(row)">编辑</el-dropdown-item>
+                          <el-dropdown-item @click="handleClickDelete(row)">删除</el-dropdown-item>
+                        </el-dropdown-menu>
+                      </template>
+                    </el-dropdown>
+                  </div>
                 </div>
               </div>
-            </div>
-            <p v-if="row?.genHost && row?.genPort" class="list-card-item_detail--desc text-text_color_regular pt-[8px] !h-[24px]">{{ row?.genHost }}:{{ row.genPort }}</p>
-            <p class="list-card-item_detail--name text-text_color_primary">
-              <span>{{ row?.genName }}</span>
-              <span v-if="row.isFileDriver == true" class="text-gray-400 text-sm pl-10">
-                {{ row.genDatabaseFileName }}
-              </span>
-            </p>
-            <div class="flex flex-1 pt-2 justify-end">
-              <ScLazy :time="200">
-                <el-button v-if="row.isFileDriver" size="small" circle :icon="useRenderIcon('ri:upload-2-line')" title="上传数据文件" @click="handleUploadDataFile(row)" />
-                <el-button v-if="row.isFileDriver && row.genDatabaseFile" size="small" circle :icon="useRenderIcon('ri:close-large-fill')" title="清除数据文件" @click="handleClearDataFile(row)" />
-                <el-button v-if="row.genJdbcCustomType == 'JDBC'" size="small" circle :icon="useRenderIcon('humbleicons:code')" title="代码" @click="handleOpenCode(row)" />
-                <el-button v-if="row.supportDocument" size="small" circle :icon="useRenderIcon('humbleicons:documents')" title="文档" @click="handleOpenDocument(row)" />
-                <el-button v-if="row?.genBackupStatus == 0 && row.supportBackup" size="small" circle :icon="useRenderIcon('ri:lock-unlock-line')" title="开启备份" @click="handleOpenBackup(row)" />
-                <el-button v-else-if="row.supportBackup" size="small" circle :icon="useRenderIcon('ri:lock-2-line')" title="停止备份" @click="handleCloseBackup(row)" />
-              </ScLazy>
+              <p v-if="row?.genHost && row?.genPort" class="list-card-item_detail--desc text-text_color_regular pt-[8px] !h-[24px]">{{ row?.genHost }}:{{ row.genPort }}</p>
+              <p class="list-card-item_detail--name text-text_color_primary">
+                <span>{{ row?.genName }}</span>
+                <span v-if="row.isFileDriver == true" class="text-gray-400 text-sm pl-10">
+                  {{ row.genDatabaseFileName }}
+                </span>
+              </p>
+              <div class="flex flex-1 pt-2 justify-end">
+                <ScLazy :time="200">
+                  <el-button v-if="row.isFileDriver" size="small" circle :icon="useRenderIcon('ri:upload-2-line')" title="上传数据文件" @click="handleUploadDataFile(row)" />
+                  <el-button v-if="row.isFileDriver && row.genDatabaseFile" size="small" circle :icon="useRenderIcon('ri:close-large-fill')" title="清除数据文件" @click="handleClearDataFile(row)" />
+                  <el-button v-if="row.genJdbcCustomType == 'JDBC'" size="small" circle :icon="useRenderIcon('humbleicons:code')" title="代码" @click="handleOpenCode(row)" />
+                  <el-button v-if="row.supportDocument" size="small" circle :icon="useRenderIcon('humbleicons:documents')" title="文档" @click="handleOpenDocument(row)" />
+                  <el-button v-if="row?.genBackupStatus == 0 && row.supportBackup" size="small" circle :icon="useRenderIcon('ri:lock-unlock-line')" title="开启备份" @click="handleOpenBackup(row)" />
+                  <el-button v-else-if="row.supportBackup" size="small" circle :icon="useRenderIcon('ri:lock-2-line')" title="停止备份" @click="handleCloseBackup(row)" />
+                </ScLazy>
+              </div>
             </div>
           </div>
-        </div>
-      </template>
-    </ScCard>
-    <ScLazy :time="300">
-      <save v-if="visible.saveVisible" ref="saveRef" @success="handlerSuccess" />
-      <Document v-if="visible.documentVisible" ref="documentRef" />
-      <Code v-if="visible.codeVisible" ref="codeRef" />
-      <File ref="fileRef" @success="handlerSuccess" />
-    </ScLazy>
+        </template>
+      </ScCard>
+      <ScLazy :time="300">
+        <save v-if="visible.saveVisible" ref="saveRef" @success="handlerSuccess" />
+        <Document v-if="visible.documentVisible" ref="documentRef" />
+        <Code v-if="visible.codeVisible" ref="codeRef" />
+        <File ref="fileRef" @success="handlerSuccess" />
+      </ScLazy>
+    </div>
   </div>
 </template>
 <script setup>
@@ -346,6 +348,7 @@ const onSave = async (row, mode) => {
   }
 
   &__disabled {
+
     .list-card-item_detail--name,
     .list-card-item_detail--desc {
       color: var(--el-text-color-disabled);
