8bf58f1b1cce5d1a51f45c238cc7802db145ebcc
yemen <1@qq.com>
2025-06-06 20:02:28 +0800

    refactor(prometheus): 优化 Prometheus 配置接口
    - 为 fetchPrometheusListConfig 和 fetchPrometheusShareConfig 接口添加了返回值类型
    - 为 fetchPrometheusSaveConfig、fetchPrometheusUpdateConfig 和 fetchPrometheusDeleteConfig 接口的参数添加了类型
    - 新增了 MonitorSysGenPrometheusConfig 接口，用于定义 Prometheus 配置的结构

--- apps/vue-support-monitor-starter/src/api/prometheus/config.ts
+++ apps/vue-support-monitor-starter/src/api/prometheus/config.ts
@@ -4,32 +4,51 @@ import { http, type ReturnResult } from "@repo/utils";
  * 获取prometheus配置
  */
 export const fetchPrometheusListConfig = (params: any) => {
-  return http.request<ReturnResult<boolean>>("get", "/v2/prometheus/config/list", { params });
+  return http.request<ReturnResult<MonitorSysGenPrometheusConfig[]>>("get", "/v2/prometheus/config/list", { params });
 };
 /**
  * 共享prometheus配置
  */
 export const fetchPrometheusShareConfig = (params: any) => {
-  return http.request<ReturnResult<boolean>>("get", "/v2/prometheus/config/share", { params });
+  return http.request<ReturnResult<MonitorSysGenPrometheusConfig[]>>("get", "/v2/prometheus/config/share", { params });
 };
 
 /**
  * 保存prometheus配置
  */
-export const fetchPrometheusSaveConfig = (params: any) => {
+export const fetchPrometheusSaveConfig = (params: MonitorSysGenPrometheusConfig) => {
   return http.request<ReturnResult<boolean>>("post", "/v2/prometheus/config/save", { data: params });
 };
 
 /**
  * 更新prometheus配置
  */
-export const fetchPrometheusUpdateConfig = (params: any) => {
+export const fetchPrometheusUpdateConfig = (params: MonitorSysGenPrometheusConfig) => {
   return http.request<ReturnResult<boolean>>("put", "/v2/prometheus/config/update", { data: params });
 };
 
 /**
  * 删除prometheus配置
  */
-export const fetchPrometheusDeleteConfig = (params: any) => {
+export const fetchPrometheusDeleteConfig = (params: MonitorSysGenPrometheusConfig) => {
   return http.request<ReturnResult<boolean>>("delete", "/v2/prometheus/config/delete", { params });
 };
+
+export interface MonitorSysGenPrometheusConfig {
+  createName: string;
+  createBy: number;
+  createTime: string;
+  updateTime: string;
+  updateName: string;
+  updateBy: number;
+  monitorSysGenPrometheusConfigId: number;
+  monitorSysGenPrometheusConfigName: string;
+  monitorSysGenPrometheusConfigPostion: string;
+  monitorSysGenPrometheusConfigQl: string;
+  monitorSysGenPrometheusConfigChartType: string;
+  monitorSysGenPrometheusConfigEnable: boolean;
+  monitorSysGenPrometheusConfigType: string;
+  monitorSysGenId: number;
+  monitorSysGenPrometheusConfigShare: boolean;
+  monitorSysGenPrometheusConfigOrigin: string;
+}
