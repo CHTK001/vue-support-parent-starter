57b054ffc201d6809957431e8235359516973d2f
yemen <1@qq.com>
2025-12-09 23:35:32 +0800

    1

---
+++ apps/vue-support-monitor-starter/src/router/modules/file-manager.ts
@@ -0,0 +1,33 @@
+/**
+ * 文件管理路由配置
+ * @author CH
+ * @date 2024-12-09
+ * @version 1.0.0
+ */
+import { $t } from "@repo/config";
+
+export default [
+  {
+    path: "/file-manager",
+    name: "fileManager",
+    meta: {
+      icon: "ri:folder-open-line",
+      title: "文件管理",
+      showLink: true,
+      rank: 9,
+    },
+    children: [
+      {
+        path: "/file/manager",
+        name: "fileManagerIndex",
+        component: () => import("@/views/file-manager/index.vue"),
+        meta: {
+          icon: "ri:file-list-3-line",
+          title: "文件存储",
+          showLink: true,
+          showParent: true,
+        },
+      },
+    ],
+  },
+];
