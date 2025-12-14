c5f4763126cd0db5adb64ee50c1cc5f9d88cc886
yemen <784412312@qq.com>
2025-12-14 23:04:17 +0800

    feat(lay-setting): 重构为主题组件模式，支持春节主题切换
    - 将原 index.vue 重构为 themes/BaseSetting.vue 基础组件
    - 创建 themes/Default.vue 默认主题组件
    - 创建 themes/SpringFestival.vue 春节主题组件
    - 添加 themes/default.scss 默认主题样式
    - 添加 themes/spring-festival.scss 春节主题样式
    - 新建 index.vue 入口文件，使用 useThemeComponent 实现主题切换
    - 参考 lay-navbar/lay-tag 的组件化主题模式

---
+++ layout/default/src/components/lay-setting/themes/Default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BaseSetting from './BaseSetting.vue';
+</script>
+
+<template>
+  <div class="default-setting-wrapper">
+    <BaseSetting />
+  </div>
+</template>
+
+<style lang="scss">
+@import './default.scss';
+</style>
